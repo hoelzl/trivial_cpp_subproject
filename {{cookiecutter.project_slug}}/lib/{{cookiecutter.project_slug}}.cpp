5d90b1376f3c898c1d6aa9fd203b8170779a7498
Matthias Hölzl <tc@xantira.com>
2025-07-01 23:43:04 +0200

    Update subproject layout to most recent version

--- {{cookiecutter.project_slug}}/lib/{{cookiecutter.project_slug}}.cpp
+++ {{cookiecutter.project_slug}}/lib/{{cookiecutter.project_slug}}.cpp
@@ -1,14 +1,15 @@
+#include <iostream>
+
 #include "{{cookiecutter.project_slug}}.h"
 
-#include <iostream>
 
 namespace {{cookiecutter.project_namespace}} {
 
 void print_{{cookiecutter.project_slug}}_info()
 {
-	std::cout << "Subproject Info\n\n";
-	std::cout << "  name:      {{cookiecutter.project_slug}}\n";
-	std::cout << "  namespace: {{cookiecutter.project_namespace}}\n";
+	std::cout << "== Program Info ==\n"
+	          << "  name:      {{cookiecutter.project_slug}}\n"
+	          << "  namespace: {{cookiecutter.project_namespace}}\n";
 }
 
 } // namespace {{cookiecutter.project_namespace}}
---
+++ {{cookiecutter.project_slug}}/lib/{{cookiecutter.project_slug}}.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef LIB_{{cookiecutter.project_slug.upper()}}_H
+#define LIB_{{cookiecutter.project_slug.upper()}}_H
+
+namespace {{cookiecutter.project_namespace}}
+{
+
+void print_{{cookiecutter.project_slug}}_info();
+
+} // namespace {{cookiecutter.project_namespace}}
+#endif // LIB_{{cookiecutter.project_slug.upper()}}_H
